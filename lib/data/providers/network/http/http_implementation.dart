c7ba9cc399f463431acd57a138bec2e4dc7d62cd
Riski Muhammad <riskimuhammad8921@gmail.com>
2024-03-27 13:02:10 +0700

    setup provider network

---
+++ lib/data/providers/network/http/http_implementation.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/src/response.dart';
+import 'package:test_muhammad_riski/data/providers/network/http/http_contract.dart';
+import 'package:test_muhammad_riski/data/providers/network/model/api_results.dart';
+import 'package:test_muhammad_riski/data/providers/network/model/network_exception.dart';
+import 'package:test_muhammad_riski/data/providers/network/model/network_model.dart';
+import 'package:http/http.dart' as http;
+
+class HttpImplementation implements HttpNetwork {
+  @override
+  ApiResult unknownException = const ApiResult.failure(
+      networkException: NetworkException.httpException());
+
+  @override
+  Future<Response> getResponse({required NetworkModel method}) async {
+    try {
+      return await method.when(
+        post: (networkParameter) async {
+          http.Response response = await http.post(
+              Uri.parse(networkParameter.url),
+              body: jsonEncode(networkParameter.requestBody),
+              headers: {...?networkParameter.header});
+          return response;
+        },
+        get: (networkParameter) async {
+          http.Response response = await http.get(
+              Uri.parse(networkParameter.url),
+              headers: {...?networkParameter.header});
+          return response;
+        },
+      );
+    } catch (exception, stacktrace) {
+      if (kDebugMode) print(stacktrace);
+      rethrow;
+    }
+  }
+
+  @override
+  bool validStatusCode(int statusCode) {
+    return statusCode >= 200 && statusCode < 300;
+  }
+}
