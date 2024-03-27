e76220eea7c26e9ad224b69b00bb80f0b875b126
Riski Muhammad <riskimuhammad8921@gmail.com>
2024-03-27 13:55:16 +0700

    create sigin model

---
+++ lib/data/models/signin_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signin_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$SigninModelImpl _$$SigninModelImplFromJson(Map<String, dynamic> json) =>
+    _$SigninModelImpl(
+      username: json['username'] as String,
+      password: json['password'] as String,
+    );
+
+Map<String, dynamic> _$$SigninModelImplToJson(_$SigninModelImpl instance) =>
+    <String, dynamic>{
+      'username': instance.username,
+      'password': instance.password,
+    };
